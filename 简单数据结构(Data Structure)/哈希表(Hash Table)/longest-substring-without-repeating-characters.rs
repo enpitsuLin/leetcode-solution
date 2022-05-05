a1fc0d42a8820b5fa67e35102fb317419c57abdf
enpitsulin <enpitsulin@gmail.com>
2022-05-05 11:59:01 +0800

    优化

--- 简单数据结构(Data Structure)/哈希表(Hash Table)/longest-substring-without-repeating-characters.rs
+++ 简单数据结构(Data Structure)/哈希表(Hash Table)/longest-substring-without-repeating-characters.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 impl Solution {
-    pub fn length_of_longest_substring(s: String) -> i32 {
+    /* pub fn length_of_longest_substring(s: String) -> i32 {
         let (mut ans, mut cnt) = (0, 0);
         let mut map = HashMap::new();
         let s = s.chars().collect::<Vec<_>>();
@@ -24,5 +24,21 @@ impl Solution {
             map.insert(*c, i);
         });
         ans as i32
+    } */
+    // 优化
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        if s.is_empty() {
+            return 0;
+        }
+        let mut ret = 0;
+        let mut l = 0;
+        let mut cache = vec![0; 128];
+
+        s.chars().enumerate().for_each(|(i, ch)| {
+            l = l.max(cache[ch as usize]);
+            ret = ret.max(i as i32 - l + 1);
+            cache[ch as usize] = i as i32 + 1;
+        });
+        ret
     }
 }
